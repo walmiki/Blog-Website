054356b570b9a16321007b4b8729131b16a817b1
walmiki <walmiki240@gmail.com>
2020-05-31 16:01:44 +0530

    update Port

--- app.js
+++ app.js
@@ -78,15 +78,11 @@ app.get("/posts/:postid", function(req,res){
 
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
